12edb0fbff2589079e94e26654a33442323254d9
Kareem00101 <89580954+Kareem00101@users.noreply.github.com>
2021-08-27 11:38:07 +0200

    sent full html pages
    handled error in routes

--- clientAndServers/server.js
+++ clientAndServers/server.js
@@ -1,22 +1,46 @@
 const http = require('http');
+const myFileSystem = require('fs');
 
 const server = http.createServer((request,response)=>{
     console.log('the server has received the request!');
+    
     // to check the request object
-    console.log(request);
+    //console.log(request);
     // set header content type
+
     // to send plain text
     /* response.setHeader('content-type', 'text/plain');
     response.write('hello, it is kareem here!');
     response.end(); */
+
     // to send html
     /* response.setHeader('content-type', 'text/html');
     response.write('<h1>WELCOME TO KA SITE</h1>');
     response.write('<p>hello! this is kareem`s site!</p>')
     response.end(); */
+
     // to send a full html page
+    response.setHeader('content-type','text/html');
+    let path = './pages/';
+    switch(request.url){
+        case '/': path += 'html1.html';
+        break;
+        case '/about': path += 'about.html';
+        break;
+        default: path+= '404.html';
+        break;
+    }
+    myFileSystem.readFile(path, (err, data)=>{
+        if(err){
+            console.log(err);
+            response.end();
+        } else {
+            response.write(data);
+            response.end();
+        }
+    });
 });
 
-server.listen(3000, 'localhost', ()=>{
-    console.log('server listening for requests, port 3000')
+server.listen(3001, 'localhost', ()=>{
+    console.log('server listening for requests, port 3001')
 });
