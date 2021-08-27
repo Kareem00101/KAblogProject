c9aadfe34d82dd2ad47d5ac3265a45d8b72fdee0
Kareem00101 <89580954+Kareem00101@users.noreply.github.com>
2021-08-27 10:52:14 +0200

    tested server request and response.
    also created an empty html file, so that we try to send to response with it, later on.

--- clientAndServers/server.js
+++ clientAndServers/server.js
@@ -2,9 +2,21 @@ const http = require('http');
 
 const server = http.createServer((request,response)=>{
     console.log('the server has received the request!');
-
+    // to check the request object
+    console.log(request);
+    // set header content type
+    // to send plain text
+    /* response.setHeader('content-type', 'text/plain');
+    response.write('hello, it is kareem here!');
+    response.end(); */
+    // to send html
+    /* response.setHeader('content-type', 'text/html');
+    response.write('<h1>WELCOME TO KA SITE</h1>');
+    response.write('<p>hello! this is kareem`s site!</p>')
+    response.end(); */
+    // to send a full html page
 });
 
 server.listen(3000, 'localhost', ()=>{
-    console.log('server listening for requests, port 4000')
+    console.log('server listening for requests, port 3000')
 });
